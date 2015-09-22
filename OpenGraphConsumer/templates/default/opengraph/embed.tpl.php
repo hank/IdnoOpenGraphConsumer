c9377e5f1e60adb8178b1e65e345c673e42a0460
hank <hank@jointsecurityarea.org>
2015-09-22 03:00:19 -0400

    Fixing issue where URL is wrong because OpenGraph didn't get one

--- OpenGraphConsumer/templates/default/opengraph/embed.tpl.php
+++ OpenGraphConsumer/templates/default/opengraph/embed.tpl.php
@@ -6,7 +6,7 @@ if (preg_match_all('/\s+(https?:\/\/[^\s]+)/i', $body, $matches)) {
     foreach ($matches[1] as $m) {
         // Parse with opengraph
         $graph = OpenGraph::fetch($m);
-        $embedded .= "<a href=\"{$graph->url}\"><div>";
+        $embedded .= "<a href=\"{$m}\"><div>";
         if($graph->image) {
             $embedded .= "<span class=\"OpenGraphImage\"style=\"float:left\">" .
                             "<img width=\"100px\"src=\"{$graph->image}\"/></span>";
